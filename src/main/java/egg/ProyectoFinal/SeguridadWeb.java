96f66cd1dd2e88cf2920bac1619750dd5d5ccf1b
DouglasGuacaran <douglasguacaran@gmail.com>
2025-03-27 14:48:52 -0300

    Ajuste en SeguridadWebJava y crear DAO

--- src/main/java/egg/ProyectoFinal/SeguridadWeb.java
+++ src/main/java/egg/ProyectoFinal/SeguridadWeb.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,7 +22,14 @@ public class SeguridadWeb {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(usuarioServicio).passwordEncoder(new BCryptPasswordEncoder());
+        auth.authenticationProvider(authenticationProvider());
+    }
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(usuarioServicio);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
     }
 
     @Bean
